506a871c8bea57aa05977cdeee01e3e7c9c805e8
xjh <xjh9800@126.com>
2022-07-09 23:40:30 +0800

    Initial commit

---
+++ src/main/java/chapter05/TransformMapTest.java
@@ -0,0 +1,32 @@
+package chapter05;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class TransformMapTest {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        //从元素中读取数据
+        DataStreamSource<Event> stream = env.fromElements(new Event("Mary","./home",1000l),
+                new Event("Bob","./cart",2000l),
+                new Event("Alice","./prod?id",3000l));
+
+        //进行转换计算
+        SingleOutputStreamOperator<String> result = stream.map(new MyMapper());
+        result.print();
+        env.execute();
+
+
+    }
+    //自定义MapFunction
+    public static class MyMapper implements MapFunction <Event,String>{
+
+        public String map(Event event) throws Exception {
+            return event.user;
+        }
+    }
+}
