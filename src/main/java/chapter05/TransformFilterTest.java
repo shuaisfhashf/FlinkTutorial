d8b696a2b1a46ad616826b3691a708915fa99f91
xjh <xjh9800@126.com>
2022-07-17 17:06:21 +0800

    提交

---
+++ src/main/java/chapter05/TransformFilterTest.java
@@ -0,0 +1,35 @@
+package chapter05;
+
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class TransformFilterTest {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        DataStreamSource<Event> stream = env.fromElements(new Event("Mary","./home",1000l),
+                new Event("Bob","./cart",2000l),
+                new Event("Alice","./prod?id",3000l))
+                ;
+        //传入了一个实现了FilterFunction的类的对象
+        SingleOutputStreamOperator<Event> filter = stream.filter(new MyFilter());
+        //2.传入一个匿名类实现FilterFunction接口
+        stream.filter(new FilterFunction<Event>() {
+            public boolean filter(Event event) throws Exception {
+                return event.user.equals("Mary");
+            }
+        })
+        filter.print();
+        env.execute();
+
+    }
+    //实现一个自定义的FilterFunction
+    public static class MyFilter implements FilterFunction<Event>{
+
+        public boolean filter(Event event) throws Exception {
+            return event.user.equals("Mary");
+        }
+    }
+}
